6f93586060ab1f096e0383280f9f627ac42898ba
duongtrinhson <87305372+duongtrinhson@users.noreply.github.com>
2023-03-30 09:33:07 +0700

    tuan3

---
+++ duongtrinhson_2011063038_lab4/Migrations/202303281832193_AddFollowingsTable1.cs
@@ -0,0 +1,16 @@
+﻿namespace duongtrinhson_2011063038_lab4.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddFollowingsTable1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
